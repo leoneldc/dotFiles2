0e4872260f0841da485b135ceb4877aef8a924e3
Leonel Dominguez <60021809+leoneldc@users.noreply.github.com>
2024-08-08 21:53:07 -0600

    Create bspwm.sh

---
+++ config/scipts/bspwm.sh
@@ -0,0 +1,6 @@
+sudo apt-get install bspwm -y
+
+mkdir ~/.config/bspwm ~/.config/sxhkd
+
+cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
+cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
