e342f299bce2e3465d5e4b41a7213671002a038c
Leonel Dominguez <60021809+leoneldc@users.noreply.github.com>
2024-08-09 10:12:57 -0600

    Update bspwm.sh

--- config/scipts/bspwm.sh
+++ config/scipts/bspwm.sh
@@ -4,3 +4,4 @@ mkdir ~/.config/bspwm ~/.config/sxhkd
 
 cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
 cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
+chmod +x ~/.config/bspwm/*
