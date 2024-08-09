43eb2f4368c83f0267ddfe5c32df2bd16fb94171
Leonel Dominguez <60021809+leoneldc@users.noreply.github.com>
2024-08-08 21:39:52 -0600

    Update sddm.sh

--- config/scipts/sddm.sh
+++ config/scipts/sddm.sh
@@ -1,6 +1,8 @@
 sudo apt install sddm -y
+sudo apt-get install build-essential libgl1-mesa-dev -y
+sudo apt-get install qml-module-qtquick-controls qml-module-qtquick-controls2 -y
 sudo dpkg-reconfigure sddm
-
+ 
 #SI TENEMOS OTRO SESIÓN MANAGER HAY QUE DESINSTALARLO
 #systemctl stop gdm3.service
 #sudo apt remove gdm3
@@ -8,3 +10,4 @@ sudo dpkg-reconfigure sddm
 sudo cp -r ~/.config/sddm/game-rpg/ /usr/share/sddm/themes
 cp ~/.config/sddm/sddm.conf /etc/
 echo "SDDM INSTALADO Y CONFIGURADO CORRECTAMENTE"
+udo apt-get install build-essential libgl1-mesa-dev
