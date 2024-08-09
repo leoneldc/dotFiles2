0c62f0aa87bd40b48cdac40de8d0a63e8b03bfca
Leonel Dominguez <60021809+leoneldc@users.noreply.github.com>
2024-08-08 21:30:55 -0600

    Create firaCode.sh

---
+++ config/scipts/firaCode.sh
@@ -0,0 +1,8 @@
+sudo apt intall wget -y
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraCode.zip -O FiraCode.zip
+unzip FiraCode.zip -d FiraCode
+mkdir -p /usr/local/share/fonts/
+cp FiraCode/*.ttf /usr/local/share/fonts/
+fc-cache -fv
+rm -rf FiraCode.zip FiraCode
+echo "FIRACODE INSTALADO Y CONFIGURADO CORRECTAMENTE"
