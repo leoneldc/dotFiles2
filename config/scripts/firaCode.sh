86796130160bc72d06dd17439038aa9bb89cda2f
Leonel Dominguez <60021809+leoneldc@users.noreply.github.com>
2024-11-02 17:17:56 -0600

    update firacode script

--- config/scripts/firaCode.sh
+++ config/scripts/firaCode.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
 
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraCode.zip -O FiraCode.zip
+
 unzip FiraCode.zip -d FiraCode
+
 mkdir -p /usr/local/share/fonts/
-cp FiraCode/*.ttf /usr/local/share/fonts/
+
+sudo cp FiraCode/*.ttf /usr/local/share/fonts/
+
 fc-cache -fv
+
 rm -rf FiraCode.zip FiraCode
+
 echo "FIRACODE INSTALADO Y CONFIGURADO CORRECTAMENTE"
--- config/scripts/oh-my-zsh.sh
+++ config/scripts/oh-my-zsh.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+sudo apt install zsh -y
+
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "OH-MY-ZSH INSTALADO Y CONFIGURADO CORRECTAMENTE"
