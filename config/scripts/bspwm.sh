d61abfd4838a013b886a0e321933d3a9e8d0adab
Leonel Dominguez <60021809+leoneldc@users.noreply.github.com>
2024-11-02 18:40:53 -0600

    update bspwm script

--- config/scripts/bspwm.sh
+++ config/scripts/bspwm.sh
@@ -4,9 +4,10 @@ sudo apt-get install bspwm -y
 
 mkdir ~/.config/bspwm ~/.config/sxhkd
 
+chmod +x ../bspwm/bspwmrc
+
 cp ../bspwm/bspwmrc ~/.config/bspwm/
 cp ../sxhkd/sxhkdrc ~/.config/sxhkd/
 
-chmod +x ~/.config/bspwm/
 
 echo "BSPWM y SXHKD INSTALADO Y CONFIGURADO CORRECTAMENTE"
