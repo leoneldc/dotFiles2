9c977036af97264eb186dd67c5331b800559cc62
Leonel Dominguez <60021809+leoneldc@users.noreply.github.com>
2024-12-14 12:17:49 -0600

    PICOM ADD

---
+++ config/scripts/picom.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+sudo apt install picom -y
+
+cp ../picom/ ~/.config/
+
+echo "PICOM INSTALADO"
+echo "AGREGAR AL ARCHIVO .conf/bspwm/bspwmrc LO SIGUIENTE:"
+echo "pkill picom"
+echo 'picom -f --daemon --config "${HOME}/.config/picom/picom.sample.conf" &'
